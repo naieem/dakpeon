4f46922960eebfbbca716f81f28a89c0c035b2b4
naieem <naieemsupto@gmail.com>
2017-12-18 23:33:33 +0600

    bootstrap js is now commented for testing purpose

--- www/js/app.js
+++ www/js/app.js
@@ -2,19 +2,20 @@
   w.registerAction('bootstrap', bootstrapDakpeon);
 
   function bootstrapDakpeon() {
+alert("sdf");
     // ==========================================
     // bootstrapping main module and application
     // ==========================================
-    w.load("js/bootstrap.js");
+    //w.load("js/bootstrap.js");
     // ================================================================
     // some pause for making dom ready to bootstrap application module
     // ================================================================
-    setTimeout(function() {
-      var moduleContainer = document.getElementById('load_module');
-      angular.element(moduleContainer).ready(function() {
-        angular.bootstrap(document, [w.appName]);
-      });
-    }, 1000);
+    // setTimeout(function() {
+    //   var moduleContainer = document.getElementById('load_module');
+    //   angular.element(moduleContainer).ready(function() {
+    //     angular.bootstrap(document, [w.appName]);
+    //   });
+    // }, 1000);
   }
 
 })(window);
